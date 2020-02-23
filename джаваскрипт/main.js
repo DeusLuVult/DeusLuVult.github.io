2b539f32c93a194ce4fb87037afad1a30b90fef6
DeusLuVult <56223630+DeusLuVult@users.noreply.github.com>
2020-02-23 17:53:42 +0200

    Add files via upload

---
+++ джаваскрипт/main.js
@@ -0,0 +1,31 @@
+// let k1='5px'  ,
+//     k2='12djd334'  ,
+//     k3='12.45asdwe12' ,
+//     k4='qwqweeewq';
+//     console.log(Number.parseInt(k1)); 
+//     console.log(Number.parseInt(k2)); 
+//     console.log(Number.parseInt(k3)); 
+//     console.log(Number.parseInt(k4)); 
+
+// let k1='5px'  ,
+    // k2='12djd334'  ,
+    // k3='12.45asdwe12' ,
+    // k4='qwqweeewq';
+    // console.log(Number.parseFloat(k1)); 
+    // console.log(Number.parseFloat(k2)); 
+    // console.log(Number.parseFloat(k3)); 
+    // console.log(Number.parseFloat(k4)); 
+
+// let random ;
+//     console.log(Math.random(random) * ((10 - 1) + 1)**2);
+
+// let a1='My ',
+//     a2='name',
+//     a3=' is',
+//     FullName=a1+a2+a3;
+//     FullName=FullName+' Viktor';
+// console.log(FullName);
+
+// result = 5 + 5+'5';
+// console.log(result); 
+// console.log(typeof result); 
