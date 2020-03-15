443e48629cccf1d66e5ac6324963e4163e1a8189
DeusLuVult <56223630+DeusLuVult@users.noreply.github.com>
2020-03-15 17:44:18 +0200

    Add files via upload

---
+++ Дважаскрипттыыы/main.js
@@ -0,0 +1,65 @@
+//1
+// let sum=0;
+//     let  arr1=[1, 5, '4','hello'];
+//     let  arr2=[true,2, {}, ['a'], 222];
+//   sum=arr1[1]+arr2[1];
+// console.log(sum);
+//2           
+// let arr1=[1,2,4,76,87]
+// arr1.push(33)
+// console.log(arr1)
+//5
+// const message = 'Welcome to Ukraine!';
+// console.log(message.split(' '));
+//4
+//let result = 0;
+// const matrix =  [
+//     [10,11,12],
+//     [13,14,15],
+//     [16,17,18],
+// ];
+// for (let i = 0; i < matrix.length; i += 1) {
+//     console.log (matrix[i]);
+  
+//     for (let j = 0; j < matrix[i].length; j += 1) {
+//       console.log(matrix[i][j]);
+//       result += matrix[i][j];
+//     }
+//   }
+//6
+// let stack=[33,44,55];
+// stack.unshift(2);
+// stack.unshift(3);
+// stack.unshift(4);
+// stack.push(5);
+// stack.push(6);
+// stack.push(7);
+// stack.push(8);
+// console.log(stack)
+//7
+// let stack=[8,2,1,33,44,55];
+// let lastNumbers=[];
+// console.log(stack);
+// for (let i=2; i<5; i++) {
+//     lastNumbers.push(stack.pop());
+//      }
+// console.log(lastNumbers);
+//8
+// let animals=['pig','dog','cat','parrot', 'chicken'];
+// let pets=[];
+// pets.push(animals.slice(1,4));
+// console.log(animals);
+// console.log(pets);
+//9
+// let pets=["dog", "cat", "parrot"];
+// pets.splice(2,1,'shark', 'elephant')
+// console.log(pets)
+
+
+
+
+
+
+
+
+
