2e53f4207d9774653b2b5b745dfc02621c106c6b
DeusLuVult <56223630+DeusLuVult@users.noreply.github.com>
2020-03-15 18:00:39 +0200

    Update main.js

--- Дважаскрипттыыы/js/main.js
+++ Дважаскрипттыыы/js/main.js
@@ -1,83 +1,60 @@
-//  let str="Привіт";
-//  let num=123 ;
-//  let boolean=true;
-//  let txt="true" ;
-// console.log (typeof(str));
-// console.log(typeof(num));
-// console.log(typeof(boolean));
-// console.log(typeof(txt));
+// //1
+// let sum=0;
+//     let  arr1=[1, 5, '4','hello'];
+//     let  arr2=[true,2, {}, ['a'], 222];
+//   sum=arr1[1]+arr2[1];
+// console.log(arr1(1));
+// console.log(arr2(1));
+// console.log(sum);
+//2           
+// let arr1=[1,2,4,76,87]
+// arr1.push(33)
+// console.log(arr1)
+//5
+// const message = 'Welcome to Ukraine!';
+// console.log(message.split(' '));
+//4
+//let result = 0;
+// const matrix =  [
+//     [10,11,12],
+//     [13,14,15],
+//     [16,17,18],
+// ];
+// for (let i = 0; i < matrix.length; i += 1) {
+//     console.log (matrix[i]);
+  
+//     for (let j = 0; j < matrix[i].length; j += 1) {
+//       console.log(matrix[i][j]);
+//       result += matrix[i][j];
+//     }
+//   }
+//6
+// let stack=[33,44,55];
+// const arr1=[2,3,4],
+//       arr2=[5,6,7,8]
+// for (let i=0; i<3; i++) {
+//         stack.unshift(arr1[i]); 
+//     }
+// for (let i=0; i<4; i++) {
+//     stack.push(arr2[i]);
+// }
+// console.log(stack)
+//7
+// let stack=[8,2,1,33,44,55];
+// let lastNumbers=[];
+// console.log(stack);
+// for (let i=2; i<5; i++) {
+//     lastNumbers.push(stack.pop());
+//      }
+// console.log(lastNumbers);
+//8
+// let animals=['pig','dog','cat','parrot', 'chicken'];
+// let pets=[];
+// pets.push(animals.slice(1,4));
+// console.log(animals);
+// console.log(pets);
+//9
+// let pets=["dog", "cat", "parrot"];
+// pets.splice(2,1,'shark', 'elephant')
+// console.log(pets)
 
-// let q=1,
-//     w=1,
-//     e=1,
-//     r=4,
-//     t=5,
-//     y=6,
-//     u=7,
-//     i=1,
-//     o=9,
-//     p=10;
-// const a=1,
-//     s=2,
-//         d=3,
-//         f=4,
-//         g=5,
-//         h=6,
-//         j=7,
-//         k=8,
-//         l=9,
-//         z=10;
-
-// let ageSvatoslav=13,
-//     agePavel=14,
-//     age=0;
-//     age=ageSvatoslav+agePavel;
-//     alert (age);
-// let answer=0;
-//     answer=108+223-2*5;
-//     console.log(answer);
-
-// let a1=5+3,
-//     a2=5-3, 
-//     a3=5*3,
-//     a4=5/3;
-//     console.log(a1);
-//     console.log(a2);
-//     console.log(a3);
-//     console.log(a4);
-
-// let a6=5%3, 
-//     a7=3%5, 
-//     a8=5 +'3', 
-//     a9='5'-3,
-//     a10=75+'кг';
-//     alert(a6)
-//     alert(a7)
-//     alert(a8)
-//     alert(a9)
-//     alert(a10)
-
-// let height=23,
-//     width=10,
-//     s=height*width
-//     alert(s)
-
-// let heightC=10,
-//     dC=4;
-//     dC=dC/2,
-//     v=3.14 * dC**2 * heightC;
-//     alert(v)
-
-// let a=3,
-//     b=4,
-//     c=0;
-//     c=Math.sqrt(a**2+b**2)
-//     alert(c)
-
-let s=2000000,
-    p=10,
-    years=5,
-    perepl=0;
-    perepl=2000000*10%;
-    perepl=perepel*5;
-    alert(perepl)
