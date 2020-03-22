019e4479216c567472ee382d4e57d658f970f8be
DeusLuVult <56223630+DeusLuVult@users.noreply.github.com>
2020-03-22 17:41:20 +0200

    Add files via upload

---
+++ Дважаскрипттыыы6/main.js
@@ -0,0 +1,33 @@
+// //1
+// function hello1(){
+//     return alert("Привiт JavaScript")
+// }
+// hello1();
+// //2
+// let name="ПАвло";
+//     function hello2(){
+//         console.log ("Привiт,"+name)
+//     }
+// hello2();
+//3
+// function mul (n,m){
+//     return alert (n+m),
+//             alert(n-m),
+//             alert(n*m);       
+// }
+// mul(2,3);
+// //4
+// function repeat (str,n){
+//     for (let i=0 ; i<n;i++){
+//         console.log(str);
+//     }
+// }
+// repeat([ 1, 2, 3],2);
+ //6
+ function m(a,b){
+    mull();
+    function mull() {
+        return console.log (a*b);
+    }
+ }
+ m(2,3);
