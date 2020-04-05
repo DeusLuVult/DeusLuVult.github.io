e474d5380b94a45a5bcca58024b843ee8dfa56d3
DeusLuVult <56223630+DeusLuVult@users.noreply.github.com>
2020-04-05 11:49:03 +0300

    Add files via upload

---
+++ Дважаскрипттыыы7/js/main.js
@@ -0,0 +1,83 @@
+//  let str="Привіт";
+//  let num=123 ;
+//  let boolean=true;
+//  let txt="true" ;
+// console.log (typeof(str));
+// console.log(typeof(num));
+// console.log(typeof(boolean));
+// console.log(typeof(txt));
+
+// let q=1,
+//     w=1,
+//     e=1,
+//     r=4,
+//     t=5,
+//     y=6,
+//     u=7,
+//     i=1,
+//     o=9,
+//     p=10;
+// const a=1,
+//     s=2,
+//         d=3,
+//         f=4,
+//         g=5,
+//         h=6,
+//         j=7,
+//         k=8,
+//         l=9,
+//         z=10;
+
+// let ageSvatoslav=13,
+//     agePavel=14,
+//     age=0;
+//     age=ageSvatoslav+agePavel;
+//     alert (age);
+// let answer=0;
+//     answer=108+223-2*5;
+//     console.log(answer);
+
+// let a1=5+3,
+//     a2=5-3, 
+//     a3=5*3,
+//     a4=5/3;
+//     console.log(a1);
+//     console.log(a2);
+//     console.log(a3);
+//     console.log(a4);
+
+// let a6=5%3, 
+//     a7=3%5, 
+//     a8=5 +'3', 
+//     a9='5'-3,
+//     a10=75+'кг';
+//     alert(a6)
+//     alert(a7)
+//     alert(a8)
+//     alert(a9)
+//     alert(a10)
+
+// let height=23,
+//     width=10,
+//     s=height*width
+//     alert(s)
+
+// let heightC=10,
+//     dC=4;
+//     dC=dC/2,
+//     v=3.14 * dC**2 * heightC;
+//     alert(v)
+
+// let a=3,
+//     b=4,
+//     c=0;
+//     c=Math.sqrt(a**2+b**2)
+//     alert(c)
+
+let s=2000000,
+    p=10,
+    years=5,
+    perepl=0;
+    perepl=2000000*10%;
+    perepl=perepel*5;
+    alert(perepl)
---
+++ Дважаскрипттыыы7/main.js
@@ -0,0 +1,32 @@
+// //1
+// const man = {
+//     name: 'Pavlo',
+//     age: 14,  
+//   gender: 'male',  
+//     married : false,
+//   };
+// //2
+// const man = {
+//   name: 'Pavlo',
+//   age: 14,
+//   gender: 'male',   
+//   married : false,
+//       };
+// delete man.name;
+//         man.language = 'JavaScript' ; 
+// console.log(man);
+//3
+const numbers = {
+
+};
+function perebor1(){
+    for(let i=1; i<11; i++){
+        for(let j=0; j<11 j++)
+        if (i%2 === 0){
+
+        }
+    }
+}
+perebor1();
+console.log (numbers);
+//4                                                       
