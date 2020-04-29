705673b6e58fdaf7edea0d6db2b5e52e48a68d1a
DeusLuVult <56223630+DeusLuVult@users.noreply.github.com>
2020-04-29 13:55:09 +0300

    Update main.js

--- Джаваскрипт--8/main.js
+++ Джаваскрипт--8/main.js
@@ -3,28 +3,28 @@
 // console.log(inputs);
 // console.log(firstInput);
 
-// number 2
+// 2
 
 // const heading = document.querySelector('.title-head');
 // heading.style.color = 'green';
 // heading.textContent = 'I LOVE MY MOM';
 // console.log(heading);
 
-// number 3
+//3
 
 // const peshka = document.querySelector('.more-text');
 // peshka.classList.add('active');
 // const pushka = document.querySelector('.new-text');
 // peshka.classList.remove('new-text');
 
-// number 4
+//4
 
 // const firstInput = document.querySelector('#inputss').value;
 // inpt = (firstInput * 4);
 // const peshka = document.querySelector('.more-text');
 // peshka.textContent = inpt;
 
-// number 5
+//5
 
 // let input = document.querySelectorAll(".inputs");
 // objGlobal = {};
@@ -34,26 +34,26 @@
 // objGlobal.pobatykovi = input[3].value;
 // console.log(objGlobal);
 
-// number 6
+//6
 
 // let img1 = document.querySelector(".image");
 // img1.src = "./img/second.jpg";
 
-// number 7
+//7
 
 // let img3 = document.querySelector(".beautiful");
 // img3.style.display = 'none';
 // let img4 = document.querySelector(".four-img");
 // img4.style.display = 'block';
 
-// number 8
+//8
 
-let check = document.querySelectorAll(".checkbox");
-for(let i = 0; i<check.length; i++){
-   if(check[i].hasAttribute('checked')){
-      check[i].removeAttribute('checked'); 
-   }
-   else{
-      check[i].setAttribute('checked', "");
-   }
-}
+//let check = document.querySelectorAll(".checkbox");
+//for(let i = 0; i<check.length; i++){
+//   if(check[i].hasAttribute('checked')){
+//      check[i].removeAttribute('checked'); 
+//  }
+//  else{
+//      check[i].setAttribute('checked', "");
+//   }
+//}
