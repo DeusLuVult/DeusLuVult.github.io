19fb8d1134344cb41f5db3608b3489a0dfeb6282
DeusLuVult <56223630+DeusLuVult@users.noreply.github.com>
2020-04-29 13:44:49 +0300

    Update main.js

--- Дважаскрипт5/main.js
+++ Дважаскрипт5/main.js
@@ -1,32 +1,56 @@
-// //1
-// const man = {
-//     name: 'Pavlo',
-//     age: 14,  
-//   gender: 'male',  
-//     married : false,
-//   };
-// //2
-// const man = {
-//   name: 'Pavlo',
-//   age: 14,
-//   gender: 'male',   
-//   married : false,
-//       };
-// delete man.name;
-//         man.language = 'JavaScript' ; 
-// console.log(man);
-//3
-const numbers = {
+ //1 
+// let Arr1 = [1, 5, "4", "hello"],
+//     Arr2 = [true, 2, {}, "a", 222];
+// console.log(Arr1[1]);
+// console.log(Arr2[1]);
+// arr3 = Arr1[1] + Arr2[1];
+// console.log(arr3);
 
-};
-function perebor1(){
-    for(let i=1; i<11; i++){
-        for(let j=0; j<11 j++)
-        if (i%2 === 0){
+// 2
 
-        }
-    }
-}
-perebor1();
-console.log (numbers);
-//4                                                       
+// let Arr1 = [1, 5, "4", "hello"];
+// Arr1.push(22);
+// for(let arr of Arr1){
+//      console.log(typeof(arr));
+// }
+
+// 3
+
+// let Arr2 = [true, 2, {}, "a", 222];
+// for(let arr of Arr2){
+//     console.log(typeof(arr));
+// }
+
+//4
+
+// const matrix = [
+//     [10, 11, 12],
+//     [13, 14, 15],
+//     [16, 17, 18]
+// ];
+// for(let i = 0; i<matrix.length; i++){
+//     console.log(matrix[i]);
+//     for(let j = 0; j < matrix[i].length; j++){
+//         console.log(matrix[i][j]);
+//     } 
+// }
+
+//5
+
+// const message = 'Welcom to Ukrain!';
+// console.log(message.split(''));
+// console.log(message.indexOf("l"));
+// console.log(message);
+
+//6
+
+// let stack = [],
+//     stok = [2, 3, 4],
+//     stk = [5, 6, 7, 8];
+// for(let i = 0; i<stok.length; i++){
+//      stack.unshift(stok[i]);  
+// }
+// for(let j = 0; j<stk.length; j++){
+//     stack.push(stk[j]);
+// }
+// console.log(stack);
