79c5a18c1fc2ad46c8e4bb3b6dc5302cb3a1537f
DeusLuVult <56223630+DeusLuVult@users.noreply.github.com>
2020-05-02 14:04:33 +0300

    Update main.js

--- Дважаскрипттыыы7/main.js
+++ Дважаскрипттыыы7/main.js
@@ -16,17 +16,26 @@
 //         man.language = 'JavaScript' ; 
 // console.log(man);
 //3
-const numbers = {
-
-};
-function perebor1(){
-    for(let i=1; i<11; i++){
-        for(let j=0; j<11 j++)
-        if (i%2 === 0){
-
-        }
-    }
-}
-perebor1();
-console.log (numbers);
-//4                                                       
+//const numbers = {
+//
+//};
+//function perebor1(){
+//    for(let i=1; i<11; i++){
+//        for(let j=0; j<11 j++)
+//        if (i%2 === 0){
+//
+//       }
+//    }
+//}
+//perebor1();
+//console.log (numbers);
+//4  
+// const hotel = {
+//     name: 'paulo',
+//     age: 14,
+//     maried: false,
+// };
+// const keys = Object.keys(hotel);
+// for(let i = 0; i< keys.length; i++){
+//     console.log(keys[i]);
+// };
